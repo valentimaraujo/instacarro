3aede20cb441cf042980c828ee07073f61ebbc9f
Valentim Araujo (Evodev) <valentim@evodev.com.br>
2021-06-14 18:13:54 -0300

    css menu top

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logoInstacarro from './assets/logo.png';
+import imgUser from './assets/user.svg';
+import imgPhone from './assets/phone.svg';
+import imgCaret from './assets/caret.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <header className="header-user">
+            <div className="header-limiter">
+                <div className="icon-brand">
+                    <a href="#">
+                        <img src={logoInstacarro} alt="Instacarro"/>
+                    </a>
+                </div>
+                <nav className="icon-phone">
+                    <img src={imgPhone} alt="(11)3569-3465" width="30px"/>
+                    (11)3569-3465
+                </nav>
+                <div className="header-user-menu">
+                    <img src={imgCaret} alt="caret" className="caret"/>
+                    <img src={imgUser} alt="user Test"/>
+                    <br />
+                    User Test
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default App;
